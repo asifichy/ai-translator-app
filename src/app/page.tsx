fd3a0ea3f3c14873f6f99dc54223259299a91efd
asifichy <asifimtiyajchowdhury@gmail.com>
2024-11-19 00:23:52 +0600

    started the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,19 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      
+        <div className="relative overflow-hidden h-screen">
+          <div className="max-w-[85rem] mx-auto px-4 sm:px-6 py-16 sm:py-24">
+            <div className="text-center">
+              <h1 className="text-4xl sm:text-6xl font-bold  text-neutral-200">
+                Interpret <span className="text-[$f87315]">AI</span>
+              </h1>
+            </div>
+          </div>
+        </div>
+      
 
     </div>
   );
