00cc75050ddcd1cd1747307200c1e348d6a8a46c
asifichy <asifimtiyajchowdhury@gmail.com>
2024-11-25 22:47:17 +0600

    adding source language input field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-//import Image from "next/image";
+"use client";
+import React, { useState, ChangeEvent } from "react";
+import TextArea from "../components/Inputs/TextArea";
 
 export default function Home() {
+
+  const [sourceText, setSourceText] = useState<string>("");
+
   return (
     <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
@@ -16,14 +21,25 @@ export default function Home() {
               <div className="mt-7 sm:mt-12 mx-auto max-w-3xl relative">
                 <div className="grid gap-4 md:grid-cols-2 grid-cols-1">
                   <div className="relative z-10 flex flex-col space-x-3 border rounded-lg shawdow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20">
-                    
+                    <TextArea
+                      id="source language"
+                      value={sourceText}
+                      onChange={(e:ChangeEvent<HTMLTextAreaElement>) => {
+                        setSourceText(e.target.value);
+                      }}                      
+                      placeholder={'Source Language'}
+                      />
+                      <div className="flex flex-row justify-between w-full">
+                        <span className="cursor-pointer flex space-x-2 flex-row">
+                          
+                        </span>
+                      </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      
+          </div>          
+        </div>      
 
     </div>
   );
