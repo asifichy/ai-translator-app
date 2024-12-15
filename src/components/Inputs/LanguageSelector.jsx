c869b8bca4eebd542af367518cdaac831d1c566d
asifichy <asifimtiyajchowdhury@gmail.com>
2024-12-15 23:47:17 +0600

    addning a chance to select languages

---
+++ src/components/Inputs/LanguageSelector.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { IconLanguage } from '@tabler/icons-react';
+
+
+const LanguageSelector = ({ selectedLanguage, setSelectedLanguage, languages }) => (
+    <span className='cursor-pointer rounded-full space-x-1 pl-2 b-black flex items-center flex-row'>
+        <IconLanguage size={22} />
+        <select
+            value={selectedLanguage}
+            onChange={(e) => setSelectedLanguage(e.target.value)}>
+                {languages.map((language) => (
+                <option key={language.code} value={language.code}>
+                    {language.name}
+                </option>
+            ))}
+        </select>
+    </span>
+
+    /*
+    const languages = [
+        { code: 'en', name: 'English' },
+        { code: 'es', name: 'Spanish' },
+        { code: 'fr', name: 'French' },
+        { code: 'de', name: 'German' },
+        { code: 'it', name: 'Italian' },
+        { code: 'pt', name: 'Portuguese' },
+        { code: 'ru', name: 'Russian' },
+        { code: 'ja', name: 'Japanese' },
+        { code: 'ko', name: 'Korean' },
+        { code: 'zh', name: 'Chinese' },
+        { code: 'ar', name: 'Arabic' },
+        { code: 'hi', name: 'Hindi' },
+        { code: 'bn', name: 'Bengali' },
+        { code: 'ur', name: 'Urdu' },
+        { code: 'fa', name: 'Persian' },
+        { code: 'tr', name: 'Turkish' },
+        { code: 'th', name: 'Thai' },
+        { code: 'id', name: 'Indonesian' },
+        { code: 'vi', name: 'Vietnamese' },
+        { code: 'el', name: 'Greek' },
+        { code: 'he', name: 'Hebrew' },
+        { code: 'nl', name: 'Dutch' },
+        { code: 'sv', name: 'Swedish' },
+        { code: 'da', name: 'Danish' }, 
+        { code: 'no', name: 'Norwegian' },
+        { code: 'fi', name: 'Finnish' },    
+        { code: 'hu', name: 'Hungarian' }, 
+    ]; */
+);
+
+export default LanguageSelector;
