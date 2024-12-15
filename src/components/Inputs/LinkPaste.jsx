eb545b9d13291cb844cea854401d788dcd6d1bf5
asifichy <asifimtiyajchowdhury@gmail.com>
2024-12-16 01:14:37 +0600

    major typo fix

--- src/components/Inputs/LinkPaste.jsx
+++ src/components/Inputs/LinkPaste.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { IconLink } from "@tabler/icons-react";
 
 const LinkPaste = ({ handleLinkPaste }) => {
-  <label htmlFor="link-input" className="cursor-pointer">
-    <IconLink size={21} />
-    <Input
-      typee="text"
-      id="link-input"
-      className="hidden"
-      onChange={handleLinkPaste}
-    />
-  </label>;
+    return (
+      <label htmlFor="link-input" className="cursor-pointer">
+        <IconLink size={21} />
+        <input
+          type="text"
+          id="link-input"
+          className="hidden"
+          onChange={handleLinkPaste}
+        />
+      </label>
+    );
 };
 
 export default LinkPaste;
