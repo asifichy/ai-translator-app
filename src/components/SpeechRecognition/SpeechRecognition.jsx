df3c96bc4d943fad116889a146d2cead471cd8d8
asifichy <asifimtiyajchowdhury@gmail.com>
2024-12-15 23:55:16 +0600

    formatting the code

--- src/components/SpeechRecognition/SpeechRecognition.jsx
+++ src/components/SpeechRecognition/SpeechRecognition.jsx
@@ -1,35 +1,34 @@
 import React, { useEffect } from "react";
-import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import SpeechRecognition, {
+  useSpeechRecognition
+} from "react-speech-recognition";
 import { IconMicrophone } from "@tabler/icons-react";
 
 const SpeechRecognitionComponent = ({ setSourceText }) => {
+  const { transcript, listening } = useSpeechRecognition();
 
-    const { transcript, listening } = useSpeechRecognition();
+  useEffect(() => {
+    setSourceText(transcript);
+  }, [transcript, setSourceText]);
 
-    useEffect(() => {
-        setSourceText(transcript);
-    }, [transcript, setSourceText]);
+  //external voice input
+  const handlingVoiceRecognition = () => {
+    if (listening) {
+      SpeechRecognition.stopListening();
+    } else {
+      SpeechRecognition.startListening();
+    }
+  };
 
-
-    //external voice input
-    const handlingVoiceRecognition = () => {
-        if (listening) {
-            SpeechRecognition.stopListening();
-        } else {
-            SpeechRecognition.startListening();
-        }
-    };
-
-    return (
-        <div>
-            <IconMicrophone 
-            size={22}
-            className="text-grey-400"
-            onClick={handlingVoiceRecognition} 
-            />
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <IconMicrophone
+        size={22}
+        className="text-grey-400"
+        onClick={handlingVoiceRecognition}
+      />
+    </div>
+  );
+};
 
 export default SpeechRecognitionComponent;
